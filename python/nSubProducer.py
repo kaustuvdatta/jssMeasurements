fda08ded36c1835a3b5045200798dec140de86aa
Alejandro <gomez@physics.rutgers.edu>
2019-08-22 10:51:12 +0200

    testing mass corrections

--- python/nSubProducer.py
+++ python/nSubProducer.py
@@ -18,11 +18,11 @@ class nsubjettinessProducer(Module):
         self.ptmin = 200
         self.maxTau = 7
 
-        
+
     def beginJob(self, histFile, histDirName):
         Module.beginJob(self, histFile, histDirName)
-        # self.beta4_W_par = [ 0.49631107, -0.91138405, 0.11273356, -0.26872, 0.04777313, 0.06521296, 0.51138633, -0.03617261] # 4-body N-subjettiness product observable params obtained via linear regression method of arXiv:1902:07180 
-        ### Observable params correspond to optimization over boosted W samples from W'->WZ(Z->vv) with pT_min=200 GeV, loose mass cut of [70,115] 
+        # self.beta4_W_par = [ 0.49631107, -0.91138405, 0.11273356, -0.26872, 0.04777313, 0.06521296, 0.51138633, -0.03617261] # 4-body N-subjettiness product observable params obtained via linear regression method of arXiv:1902:07180
+        ### Observable params correspond to optimization over boosted W samples from W'->WZ(Z->vv) with pT_min=200 GeV, loose mass cut of [70,115]
 
 
         self.nSub0p5 = ROOT.NsubjettinessWrapper( 0.5, 0.8, 0, 0 ) #beta, cone size, measureDef 0=Normalize, axesDef 0=KT_axes
@@ -51,7 +51,7 @@ class nsubjettinessProducer(Module):
         self.genMETBranchName = "GenMET"
         self.subGenJetAK8BranchName = "SubGenJetAK8"
 
-        
+
 	### Kinematics Cuts Jets ###
         self.minJetPt = 200.
         self.minJetSDMass = 40.
@@ -69,13 +69,13 @@ class nsubjettinessProducer(Module):
         self.minTightElectronPt = 120.
         self.minElectronPt = 35.
         self.range1ElectronEta = [0,1.442]
-        self.range2ElectronEta = [1.56,2.5]  
+        self.range2ElectronEta = [1.56,2.5]
         self.minElectronMET = 80.
 
         ### Kinematic Cuts b-jets ###
         self.minbJetPt = 30.
         self.maxbJetEta = 2.4
-        
+
         ### b-tag
         self.minBDisc = 0.8484
         ### Medium https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation80XReReco
@@ -105,10 +105,10 @@ class nsubjettinessProducer(Module):
             self.out.branch("goodrecojet" + ijet + "_N21",  "F")
 	    self.out.branch("dr_LepJet",  "F")
             self.out.branch("dphi_LepJet",  "F")
-	    self.out.branch("dphi_LepMet", "F")	
+	    self.out.branch("dphi_LepMet", "F")
             self.out.branch("dphi_MetJet",  "F")
             self.out.branch("dphi_WJet"  ,  "F")
-	    
+
             #self.out.branch("goodrecojet" + ijet + "_Beta_4_W",  "F")
             #print 'beginFIle', ijet
             for tauN in range(self.maxTau):
@@ -116,7 +116,7 @@ class nsubjettinessProducer(Module):
                 self.out.branch("goodrecojet" + ijet + "_tau_1_"+str(tauN),  "F")
                 self.out.branch("goodrecojet" + ijet + "_tau_2_"+str(tauN),  "F")
 	    self.out.branch("MET",  "F")
-            self.out.branch("leptonicW_pT",  "F")            
+            self.out.branch("leptonicW_pT",  "F")
 	    self.out.branch("lepton_pT",  "F")
             self.out.branch("passedMETfilters",  "I")
 	    self.out.branch("genmatchedrecoAK8",  "I")
@@ -138,17 +138,17 @@ class nsubjettinessProducer(Module):
 
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
-        
+
         isMC = event.run == 1
 
         self.dummy+=1
         #if (self.dummy > 10000): return False
         if self.verbose: print ('Event : ', event.event)
         if self.dummy%1000==0: print ("Analyzing events...", self.dummy)
-            
-	
+
+
         ### Get W->jj candidate ###
-        
+
         jets = list(Collection(event, self.jetBranchName ))
         pfCands = list(Collection(event, self.pfCandsBranchName ))
         electrons = list(Collection(event, self.electronBranchName))
@@ -157,21 +157,21 @@ class nsubjettinessProducer(Module):
         met = Object(event, self.metBranchName)
 
         #electronTrigger = event.HLT_Ele115_CaloIdVT_GsfTrkIdT
-        
+
 	if isMC==True:
 	    #list real W's, match gen-level hadronically daughting W's to selected W candidate jets at reco-level
             genParticles = Collection(event, "GenPart")
 	    genWmoms = []
 	    genWdaughts = []
-            
-            
+
+
             Tmom = [x for x in genParticles if x.pt>10 and abs(x.pdgId)==6]
             Tdaughts = [x for x in genParticles if x.pt>1 and (abs(x.pdgId)==5 or abs(x.pdgId)==24)]
 
             Wdaughts = [x for x in genParticles if x.pt>1. and 0<abs(x.pdgId)<9]
-	    
+
 	    Wmom = [x for x in genParticles if x.pt>10. and abs(x.pdgId)==24]
-   
+
 	    #if self.dummy%500==0:
 	    # 	for x in Wdaughts:
             #	    print "Wdaught:", x.pdgId, x.genPartIdxMother, x.statusFlags
@@ -188,34 +188,34 @@ class nsubjettinessProducer(Module):
                                 #self.out.fillBranch("GPIdx_W",x.genPartIdxMother)
                                 genWmoms.append(y)
                                 genWdaughts.append(x)
-                                        
+
                         except:
                             continue
 
 
         ### Applying selections
-        
+
         # applying basic selections to loose leptons, to decide on veto or not (only using muons for now)
 
         if not event.HLT_Mu50: return False
 	if not event.nMuon > 0: return False
-	if not event.nFatJet > 0: return False	
+	if not event.nFatJet > 0: return False
         muonTrigger = event.HLT_Mu50
 
 	recoElectrons  = [x for x in electrons if x.pt>self.minElectronPt and x.cutBased_HEEP and ((self.range1ElectronEta[0]<abs(x.p4().Eta())<self.range1ElectronEta[1]) or (self.range2ElectronEta[0]<abs(x.p4().Eta())<self.range2ElectronEta[1]))]
 
-	recoMuons = [ x for x in muons if x.pt > self.minMuonPt and x.highPtId > 1 and abs(x.p4().Eta()) < self.maxMuonEta and x.pfRelIso03_all < 0.1]  
+	recoMuons = [ x for x in muons if x.pt > self.minMuonPt and x.highPtId > 1 and abs(x.p4().Eta()) < self.maxMuonEta and x.pfRelIso03_all < 0.1]
 
         recoElectrons.sort(key=lambda x:x.pt, reverse=True)
         recoMuons.sort(key=lambda x:x.pt, reverse=True)
-        
+
 	if not len(recoMuons) > 0 or not recoMuons[0].pt > self.minTightMuonPt or not abs(recoMuons[0].eta) < self.maxTightMuonEta: return False
 	#if not recoMuons[0].highPtId >= 2 or not recoMuons[0].isPFcand or not recoMuons[0].pfIsoId >= 4: return False
 
         recoLepton=ROOT.TLorentzVector()
         lepflag = -1
         if len(recoMuons)+len(recoElectrons)>=1:
-            if len(recoMuons)>=1:                
+            if len(recoMuons)>=1:
                 if muonTrigger==0: return False
                 lepflag=1
                 recoLepton = recoMuons[0].p4()
@@ -229,7 +229,7 @@ class nsubjettinessProducer(Module):
             return False
         MET = ROOT.TLorentzVector()
         MET.SetPtEtaPhiE(met.pt, 0., met.phi, met.sumEt)
-	
+
 
 	passedMETFilters = False
         try:
@@ -243,24 +243,26 @@ class nsubjettinessProducer(Module):
 	#if leptonicWCand.Pt()<200: return False
         ### applying selection to fatjets, ak4jets, angular selections between lepton and ak4jet, lepton and fatjets, and ak4 and ak8 jets
 
-        recojets = [ x for x in jets if x.pt > self.minJetPt and abs(x.p4().Eta()) < self.maxJetEta and x.msoftdrop>self.minJetSDMass]
+        ##recojets = [ x for x in jets if x.pt > self.minJetPt and abs(x.p4().Eta()) < self.maxJetEta and x.msoftdrop>self.minJetSDMass]
+        recojets = [ x for x in jets if x.pt_nom > self.minJetPt and abs(x.p4().Eta()) < self.maxJetEta and x.msoftdrop_nom>self.minJetSDMass]
         recojets.sort(key=lambda x:x.pt,reverse=True)
-	
+
 
         if len(recojets)<1: #exit if no AK8jets in event
             return False
-        
+
         recoAK8 = ROOT.TLorentzVector()
-        recoAK8.SetPtEtaPhiM(recojets[0].pt,recojets[0].eta,recojets[0].phi,recojets[0].mass)
-        
+        #recoAK8.SetPtEtaPhiM(recojets[0].pt,recojets[0].eta,recojets[0].phi,recojets[0].mass)
+        recoAK8.SetPtEtaPhiM(recojets[0].pt_nom,recojets[0].eta,recojets[0].phi,recojets[0].mass_nom)
+
 	dR_AK8Lepton = recoAK8.DeltaR(recoLepton)
 	if abs(dR_AK8Lepton)<1. : return False #take care of lepton overlap
 
         ### applying basic selection to ak4-jets if any, if passed these are b-jet candidates
         recoAK4jets = [ x for x in bjets if x.pt > self.minbJetPt and abs(x.p4().Eta()) < self.maxbJetEta and x.btagCSVV2 > self.minBDisc and recoAK8.DeltaR(x.p4())>1.]
-	
+
 	if len(recoAK4jets)<1: return False #exit if no AK4-jets in event
-        
+
 
 	recoAK4 = ROOT.TLorentzVector()
 	recoAK4.SetPtEtaPhiM(recoAK4jets[0].pt, recoAK4jets[0].eta,recoAK4jets[0].phi,recoAK4jets[0].mass)
@@ -274,7 +276,7 @@ class nsubjettinessProducer(Module):
             for W in genWmoms:
                 genW_4v = ROOT.TLorentzVector()
                 genW_4v.SetPtEtaPhiM(W.pt,W.eta,W.phi,W.mass)
-                
+
                 #self.out.fillBranch("dR_AK8motherW_W", abs(recoAK8.DeltaR(genW_4v)))
                 if abs(recoAK8.DeltaR(genW_4v))<0.8:
                     #self.out.fillBranch("dR_AK8motherW_mW", abs(recoAK8.DeltaR(genW_4v)))
@@ -286,26 +288,26 @@ class nsubjettinessProducer(Module):
                         #self.out.fillBranch("dR_AK8motherW_W", abs(recoAK8.DeltaR(genW_4v)))
                         gendec_4v = ROOT.TLorentzVector()
                         gendec_4v.SetPtEtaPhiM(Wdec.pt,Wdec.eta,Wdec.phi,Wdec.mass)
-                    
+
                         if abs(recoAK8.DeltaR(gendec_4v))<0.6:
                             ndec +=1
 
-                    if ndec>1: 
+                    if ndec>1:
                         self.out.fillBranch("dR_AK8motherW_mW", abs(recoAK8.DeltaR(genW_4v)))
                         self.out.fillBranch("dR_AK8qi_mW", abs(recoAK8.DeltaR(gendec_4v)))
                         self.out.fillBranch("PID_mW", W.pdgId)
                         self.realW = 1
-                    else: 
-                        #self.out.fillBranch("dR_AK8qi_umW" abs(recoAK8.DeltaR(gendec_4v)))                     
+                    else:
+                        #self.out.fillBranch("dR_AK8qi_umW" abs(recoAK8.DeltaR(gendec_4v)))
                         self.out.fillBranch("PID_umW", W.pdgId)
-                        self.realW = 0 
+                        self.realW = 0
 
 
         pfCandsVec = ROOT.vector("TLorentzVector")()
 
         for p in pfCands :
             pfCandsVec.push_back( ROOT.TLorentzVector( p.p4().Px(), p.p4().Py(), p.p4().Pz(), p.p4().E()) )
-        
+
 
         for irecojet,recojet in enumerate(recojets):
 
@@ -319,15 +321,18 @@ class nsubjettinessProducer(Module):
                     nsub0p5 = self.nSub0p5.getTau( self.maxTau, constituents )
                     nsub1 = self.nSub1.getTau( self.maxTau, constituents )
                     nsub2 = self.nSub2.getTau( self.maxTau, constituents )
-                    
+
             if (irecojet < 1 ): #to extract only the leading jet
 
-                self.out.fillBranch("goodrecojet" + str(irecojet) + "_pt",  recojet.p4().Pt() )
+                #self.out.fillBranch("goodrecojet" + str(irecojet) + "_pt",  recojet.p4().Pt() )
+                self.out.fillBranch("goodrecojet" + str(irecojet) + "_pt",  recojet.pt_nom )
                 self.out.fillBranch("goodrecojet" + str(irecojet) + "_eta",  recojet.p4().Eta() )
                 self.out.fillBranch("goodrecojet" + str(irecojet) + "_phi",  recojet.p4().Phi() )
-                self.out.fillBranch("goodrecojet" + str(irecojet) + "_mass",  recojet.p4().M() )
-                self.out.fillBranch("goodrecojet" + str(irecojet) + "_softdrop_mass", recojet.msoftdrop)
-                self.out.fillBranch("leptonicW_pT",leptonicWCand.Pt())                	
+                #self.out.fillBranch("goodrecojet" + str(irecojet) + "_mass",  recojet.p4().M() )
+                self.out.fillBranch("goodrecojet" + str(irecojet) + "_mass",  recojet.mass_nom )
+                #self.out.fillBranch("goodrecojet" + str(irecojet) + "_softdrop_mass", recojet.msoftdrop)
+                self.out.fillBranch("goodrecojet" + str(irecojet) + "_softdrop_mass", recojet.msoftdrop_nom)
+                self.out.fillBranch("leptonicW_pT",leptonicWCand.Pt())
                 self.out.fillBranch("lepton_pT",recoLepton.Pt())
                 self.out.fillBranch("MET", met.pt)
 		self.out.fillBranch("dr_LepJet"  , abs(dR_AK8Lepton))
@@ -335,13 +340,13 @@ class nsubjettinessProducer(Module):
 		self.out.fillBranch("dphi_LepMet", abs(recoLepton.DeltaPhi(MET)))
 		self.out.fillBranch("dphi_MetJet", abs(recoAK8.DeltaPhi(MET)))
 		self.out.fillBranch("dphi_WJet" , abs(recoAK8.DeltaPhi(leptonicWCand)))
-                self.out.fillBranch("genmatchedrecoAK8",  self.realW)	
+                self.out.fillBranch("genmatchedrecoAK8",  self.realW)
 
-                if recojet.tau1 > 0.: 
+                if recojet.tau1 > 0.:
                     tau21 = recojet.tau2/recojet.tau1
                 else:
-                    tau21 = -1. 
-                    
+                    tau21 = -1.
+
                 self.out.fillBranch("goodrecojet" + str(irecojet) + "_tau21", tau21)
                 self.out.fillBranch("goodrecojet" + str(irecojet) + "_N21",  recojet.n2b1)
 		self.out.fillBranch("passedMETfilters",passedMETFilters)
@@ -350,7 +355,7 @@ class nsubjettinessProducer(Module):
                     self.out.fillBranch("goodrecojet" + str(irecojet) + "_tau_0p5_"+str(tauN),  nsub0p5[tauN]  )
                     self.out.fillBranch("goodrecojet" + str(irecojet) + "_tau_1_"+str(tauN),  nsub1[tauN]  )
                     self.out.fillBranch("goodrecojet" + str(irecojet) + "_tau_2_"+str(tauN),  nsub2[tauN]  )
-                
+
         return True
 
     ##################### Helpful functions
--- test/jetObservables_crab_extNanoAOD_MC.py
+++ test/jetObservables_crab_extNanoAOD_MC.py
@@ -13,15 +13,15 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 #this takes care of converting the input files from CRAB
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
 
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeight_2016, puAutoWeight_2016 #puWeight_2016     
-from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import btagSF2016         
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeight_2016, puAutoWeight_2016 #puWeight_2016
+from PhysicsTools.NanoAODTools.postprocessing.modules.btv.btagSFProducer import btagSF2016
 #from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jecUncertainties import jecUncertAll #jetRecalib2016BCD, jetRecalib2016BCDAK8Puppi
 
-#from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import jetmetUncertainties2016AK8PuppiAll
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetUncertainties import jetmetUncertainties2016AK8PuppiAll
 
 # our module
 from Analysis.jetObservables.nSubProducer import nsubjettinessProducer
-#from Analysis.jetObservables.nSubproducer_WP import nsubjettinessProducer     
+#from Analysis.jetObservables.nSubproducer_WP import nsubjettinessProducer
 #from Analysis.jetObservables.nSubProducer_matched_TTSemilept import nsubjettinessProducer
 #from Analysis.jetObservables.nSubProducer_matched_bbveto_TTSemilept import nsubjettinessProducer
 # Output file name, and decide if this is MC or data
@@ -88,11 +88,18 @@ print inputFiles()
 #    cuts = ptcuts
 #
 #print 'Applying cuts : ' + cuts
-p1=PostProcessor( ".", inputFiles(), "", "keep_and_drop.txt", 
-                  modules=[puWeight_2016(), btagSF2016(), nsubjettinessProducer()], 
+modulesToRun = [ puWeight_2016(),
+        jetmetUncertainties2016AK8PuppiAll(),
+        btagSF2016(),
+        nsubjettinessProducer()
+        ]
+p1=PostProcessor( ".", inputFiles(), "", "keep_and_drop.txt",
+                    modules=modulesToRun,
                     provenance=True,
                     fwkJobReport=True,
                     #jsonInput=runsAndLumis(),
+                    #prefetch=True,
+                    #maxEntries=20000,
                     haddFileName=haddname )
 
 p1.run()
