64138b0f383e1256067e1846871eba742761b609
Kaustuv Datta <kaustuv1993@protonmail.com>
2020-03-24 13:01:15 +0100

    Update nSubExtractor.py
    included input to choose axisdef+recomb. scheme for nSub bases

--- Unfolding/notebooks/nSubExtractor.py
+++ Unfolding/notebooks/nSubExtractor.py
@@ -12,14 +12,16 @@ import h5py
 class nSubExtractor:
     #M-body variable encodes max N for tau_N, 
     #selections available are only for boosted W, but more to be added soon
+    #axisdef has values "" for E-sch. recombination w/ excl. kT OR "WTA_kT" for WTA w/ excl. kT
+    #OR "OP_kT" for one-pass WTA w/ excl. kT (it defines the axisdef+recombination scheme for the nSubs)
 
-
-    def __init__(self, sample="TTbar/1", isMC=1, selection='W'):
+    def __init__(self, sample="TTbar/1", isMC=1, selection='W', axisdef=""):
 
         self.selection = selection
         self.nSub_labels = ["_tau_0p5_", "_tau_1_", "_tau_2_"]
         self.maxTau = 7
-        self.axisdef = "Echeme_excl_kT"
+        if axisdef=="": self.axisdef = axisdef 
+        else: self.axisdef = "_"+axisdef
         self.sample = sample
         #self.cuts = []
         self.sample_names = ["TTbar/1", "ST/1", "ST/2", "ST/3", "ST/4", "ST/5", "Wjets/2",
@@ -52,6 +54,7 @@ class nSubExtractor:
         else:    
             self.filepaths = ['../AutoObs/CMS/Unf_SysUnc/%s/jetObservables_nanoskim_*.root'%sample for sample in self.sample_list]
 
+    
     def create_var_sel_list(self):
         
         tau_reco = ['']
@@ -61,12 +64,12 @@ class nSubExtractor:
             for x in self.nSub_labels:
 
                 if tauN==0 and x=="_tau_0p5_": 
-                    if self.isMC: tau_gen[0] ="goodgenjet0"+x+str(tauN)
-                    tau_reco[0] ="goodrecojet0"+x+str(tauN)
+                    if self.isMC: tau_gen[0] ="goodgenjet0"+x+str(tauN)+self.axisdef
+                    tau_reco[0] ="goodrecojet0"+x+str(tauN)+self.axisdef
                     
                 else:
-                    if self.isMC: tau_gen.append("goodgenjet0"+x+str(tauN))
-                    tau_reco.append("goodrecojet0"+x+str(tauN))
+                    if self.isMC: tau_gen.append("goodgenjet0"+x+str(tauN)+self.axisdef)
+                    tau_reco.append("goodrecojet0"+x+str(tauN)+self.axisdef)
 
 
         if self.isMC:
@@ -90,8 +93,8 @@ class nSubExtractor:
                                   var_list.index('dphi_WJet'),
                                  var_list.index('passedMETfilters')]
             sel_i = selection_indices
-            tau_reco_ind = var_list.index('goodrecojet0_tau_0p5_0')
-            tau_gen_ind = var_list.index('goodgenjet0_tau_0p5_0')
+            tau_reco_ind = var_list.index('goodrecojet0_tau_0p5_0%s'%self.axisdef)
+            tau_gen_ind = var_list.index('goodgenjet0_tau_0p5_0%s'%self.axisdef)
             if 'Wjets' in self.sample or 'Wj' in self.sample:
                 weight_ind = [var_list.index('puWeight'), var_list.index('btagWeight_CSVV2'),
                               var_list.index('genWeight'),]
@@ -114,7 +117,7 @@ class nSubExtractor:
                                   var_list.index('dphi_WJet'),
                                  var_list.index('passedMETfilters')]
             sel_i = selection_indices
-            tau_reco_ind = var_list.index('goodrecojet0_tau_0p5_0')
+            tau_reco_ind = var_list.index('goodrecojet0_tau_0p5_0%s'%self.axisdef)
             #weight_ind = [var_list.index('puWeight'), var_list.index('btagWeight_CSVV2')]
             return var_list, sel_i, tau_reco_ind#, weight_ind
         return -1
@@ -184,8 +187,10 @@ class nSubExtractor:
                     k = 0
                     for z in weight_ind:
                         weights[w][k] = dataset[i][z]
+                        #print i, z, w, k
                         k = k+1
-
+                    w = w+1
+                    
             for i in evt_list:
                 y=0
                 for z in range(tau_reco_ind,tau_reco_ind+21):
